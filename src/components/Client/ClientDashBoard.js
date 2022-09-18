6d3d8109f7d4d9dac1ac3820ef6c81cbb2a3621f
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2022-09-19 04:57:44 +0530

    Update ClientDashboard.js

--- src/components/Client/ClientDashBoard.js
+++ src/components/Client/ClientDashBoard.js
@@ -11,92 +11,101 @@ import { DragDropContext,Droppable,Draggable } from "react-beautiful-dnd";
 
 const useStyle=makeStyles((theme)=>({
   
-    button: {
+  button: {
       fontSize: "20px",
-      backgroundColor: "LightGray",
-      padding: theme.spacing(0.75),
+      // backgroundColor: "LightGray",
+      // padding: theme.spacing(0.75),
       borderRadius: "5px",
-      display: "inline-block",
+      border: "10px solid #e8e8e8",
+      // borderTop: "10px solid #e8e8e8",
+      cursor: "pointer",
+      transition: "0.5s",
+      margin: "20px",
       fontWeight: 600,
-
-  },
+      "&:hover": {
+        boxShadow: "0px 5px 25px",
+    },
   
-
-}));
-
-const Home = () => {
-  const classes=useStyle();
-
-    const [jobs, setJobs] = useState([]);
-    const [loading, setloading] = useState(true);
-    const [newJobModal, setNewJobModal]= useState(false)
-    const [viewJob, setViewJob] = useState({});
-    const { logout } = useAuth()
-    const history=useHistory(); 
-    const [dragcard, updatedragCard] = useState([])
+  }
     
-
-    const fetchJobs = async () => {
-      setloading(true);
-      const req = await firestore.collection("jobs").orderBy("postedOn", "desc").get();
-       const tempJob= req.docs.map((job)=> ({ ...job.data(), id: job.id }));
-       setJobs(tempJob);
-       updatedragCard(tempJob)
-       setloading(false);
-    };
-
-   async function handleLogout(){
-    await logout()
-    history.push("/userlogin")
-}
-
-useEffect(() => {
-      fetchJobs();
-     
-}, [])
-
-
- function handleOnDragEnd(result) {
-    const items= Array.from(dragcard);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0 , reorderedItem);
-    updatedragCard(items);
-}
-
-
-  return (
-    
- <ThemeProvider className="my-1" theme={theme}>
- <Header disable={true} openNewJobModal={() => setNewJobModal(true)} />
-    <Button variant="contained" className={classes.button} bgcolor="cyan" disableElevation onClick={handleLogout}><strong>Logout</strong></Button>
-    <ViewJobModal job={viewJob} closeModal={()=>setViewJob({})}/>
-    
-    <Box mb={3}>
-    <DragDropContext onDragEnd={handleOnDragEnd} >
-        <Droppable droppableId="JobCards">
-            {(provided)=>(
-                <Grid container justify="center" {...provided.droppableProps} ref={provided.innerRef}>
-                    
-                <Grid item xs={10}> 
-                   {loading?
-                  <Box  display="flex" justifyContent="center"><CircularProgress/></Box>
-                  :dragcard.map((job,index) =>{return(<Draggable key={job.id} draggableId={job.id}  index={index}>
-                    {(provided) => (
-                        <Grid item {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                       <JobCard open={()=> setViewJob(job)}  {...job} />
-                       </Grid>
-                    )}
-                    </Draggable>)} ) }
+  
+  }));
+  
+  const Home = () => {
+    const classes=useStyle();
+  
+      const [jobs, setJobs] = useState([]);
+      const [loading, setloading] = useState(true);
+      const [newJobModal, setNewJobModal]= useState(false)
+      const [viewJob, setViewJob] = useState({});
+      const { logout } = useAuth()
+      const history=useHistory(); 
+      const [dragcard, updatedragCard] = useState([])
+      
+  
+      const fetchJobs = async () => {
+        setloading(true);
+        const req = await firestore.collection("jobs").orderBy("postedOn", "desc").get();
+         const tempJob= req.docs.map((job)=> ({ ...job.data(), id: job.id }));
+         setJobs(tempJob);
+         updatedragCard(tempJob)
+         setloading(false);
+      };
+  
+     async function handleLogout(){
+      await logout()
+      history.push("/userlogin")
+  }
+  
+  useEffect(() => {
+        fetchJobs();
+       
+  }, [])
+  
+  
+   function handleOnDragEnd(result) {
+      const items= Array.from(dragcard);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0 , reorderedItem);
+      updatedragCard(items);
+  }
+  
+  
+    return (
+      
+   <ThemeProvider className="my-1" theme={theme}>
+   <Header disable={true} openNewJobModal={() => setNewJobModal(true)} />
+        <div><center><Button display="flex" style={{marginTop: '2%'}} variant="contained" className={classes.button} onClick={handleLogout}>Lets get back to the homepage!!</Button></center>
+        </div>    
+        <ViewJobModal job={viewJob} closeModal={()=>setViewJob({})}/>
+      
+      <Box mb={3}>
+      <DragDropContext onDragEnd={handleOnDragEnd} >
+          <Droppable droppableId="JobCards">
+              {(provided)=>(
+                  <Grid container justify="center" {...provided.droppableProps} ref={provided.innerRef}>
+                      
+                  <Grid item xs={10}> 
+                     {loading?
+                    <Box  display="flex" justifyContent="center"><CircularProgress/></Box>
+                    :dragcard.map((job,index) =>{return(<Draggable key={job.id} draggableId={job.id}  index={index}>
+                      {(provided) => (
+                          <Grid item {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                         <JobCard open={()=> setViewJob(job)}  {...job} />
+                         </Grid>
+                      )}
+                      </Draggable>)} ) }
+                  </Grid>
                 </Grid>
-              </Grid>
-            )}
-    
-    </Droppable>
-    </DragDropContext>
-    </Box> 
-   
-</ThemeProvider>
-  )
-}
-
-export default Home
+              )}
+      
+      </Droppable>
+      </DragDropContext>
+      </Box> 
+     
+  </ThemeProvider>
+    )
+  }
+  
+  export default Home
+  
