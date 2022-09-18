ea556fac132b1700a7cf8034be190a1082604071
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2022-09-18 13:48:19 +0530

    Added Home, Login & Dashboard page

---
+++ src/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const dashboard = ()=>{
+    return(
+        <div>
+            Welcome to the dashboard page
+        </div>
+    )
+}
+export default dashboard;
---
+++ src/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const login = ()=>{
+    return(
+        <div>
+            Welcome to the login page
+        </div>
+    )
+}
+export default login;
